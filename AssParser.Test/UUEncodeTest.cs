b1c754fb9b6f4b4eff653b8df50454717d60a982
sun128764 <sun128764@gmail.com>
2023-08-29 21:03:53 +0800

    Add Unit test

---
+++ AssParser.Test/UUEncodeTest.cs
@@ -0,0 +1,27 @@
+using AssParser.Lib;
+namespace AssParser.Test
+{
+    public class UUEncodeTest
+    {
+        [Fact]
+        public void UUDecode_ShouldBe_Same()
+        {
+            var ttf = File.ReadAllBytes(@"Resource\FreeSans.ttf");
+            var assfile = Lib.AssParser.ParseAssFile(@"Resource\1.ass").Result;
+            var fontsdata = assfile.UnknownSections["[Fonts]"];
+            fontsdata = fontsdata.Remove(0, fontsdata.IndexOf("\n") + 1).Trim();
+            var data1 = UUEncode.Decode(fontsdata);
+            Assert.Equal(ttf, data1);
+        }
+        [Fact]
+        public void UUEncode_ShouldBe_Same()
+        {
+            var assfile = Lib.AssParser.ParseAssFile(@"Resource\1.ass").Result;
+            var fontsdata = assfile.UnknownSections["[Fonts]"];
+            fontsdata = fontsdata.Remove(0, fontsdata.IndexOf("\n") + 1).Trim();
+            var data1 = UUEncode.Decode(fontsdata);
+            var encoded = UUEncode.Encode(data1);
+            Assert.Equal(fontsdata, encoded);
+        }
+    }
+}
