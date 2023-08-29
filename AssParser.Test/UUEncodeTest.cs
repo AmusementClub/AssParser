d0b2444b4f1d09e2e77013dd282357f465f4b566
sun128764 <sun128764@gmail.com>
2023-08-29 21:11:52 +0800

    fix path

--- AssParser.Test/UUEncodeTest.cs
+++ AssParser.Test/UUEncodeTest.cs
@@ -6,8 +6,8 @@ namespace AssParser.Test
         [Fact]
         public void UUDecode_ShouldBe_Same()
         {
-            var ttf = File.ReadAllBytes(@"Resource\FreeSans.ttf");
-            var assfile = Lib.AssParser.ParseAssFile(@"Resource\1.ass").Result;
+            var ttf = File.ReadAllBytes(Path.Combine("Resource", "FreeSans.ttf"));
+            var assfile = Lib.AssParser.ParseAssFile(Path.Combine("Resource", "1.ass")).Result;
             var fontsdata = assfile.UnknownSections["[Fonts]"];
             fontsdata = fontsdata.Remove(0, fontsdata.IndexOf("\n") + 1).Trim();
             var data1 = UUEncode.Decode(fontsdata);
@@ -16,7 +16,7 @@ namespace AssParser.Test
         [Fact]
         public void UUEncode_ShouldBe_Same()
         {
-            var assfile = Lib.AssParser.ParseAssFile(@"Resource\1.ass").Result;
+            var assfile = Lib.AssParser.ParseAssFile(Path.Combine("Resource", "1.ass")).Result;
             var fontsdata = assfile.UnknownSections["[Fonts]"];
             fontsdata = fontsdata.Remove(0, fontsdata.IndexOf("\n") + 1).Trim();
             var data1 = UUEncode.Decode(fontsdata);
