1618df8fd202f7b712d12b2161c2e43a7a88de7c
TautCony <i@tautcony.xyz>
2023-08-30 17:35:45 +0800

    feat: add look up table

--- AssParser.Lib/UUEncode.cs
+++ AssParser.Lib/UUEncode.cs
@@ -4,6 +4,18 @@ namespace AssParser.Lib
 {
     public class UUEncode
     {
+        private static readonly char[] EncLUT =
+        {
+            '!', '"', '#', '$', '%', '&', '\'', '(',
+            ')', '*', '+', ',', '-', '.', '/', '0',
+            '1', '2', '3', '4', '5', '6', '7','8',
+            '9', ':', ';', '<', '=', '>', '?','@',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G','H',
+            'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+            'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+            'Y', 'Z', '[', '\\', ']', '^', '_', '`',
+        };
+
         public static string Encode(byte[] data, bool insertBr = true, bool crlf = true)
         {
             var written = 0;
@@ -14,29 +26,31 @@ namespace AssParser.Lib
                 resLength += (resLength / 80 - (resLength % 80 == 0 ? 1 : 0)) * (crlf ? 2 : 1);
             }
             var res = new char[resLength];
-            var dst = new byte[4];
+            var dst = new char[4];
             var length = data.Length;
             for (var pos = 0; pos < length; pos += 3)
             {
                 var numBytesRemain = Math.Min(length - pos, 3);
-                
-                dst[0] = (byte)(data[pos] >> 2);
-                dst[1] = 0;
+
+                dst[0] = EncLUT[(data[pos] >> 2) & 0x3f];
                 switch (numBytesRemain)
                 {
+                    case 1:
+                        dst[1] = EncLUT[(data[pos + 0] << 4) & 0x3f];
+                        break;
                     case 2:
-                        dst[1] = (byte)(((data[pos] & 0x3) << 4) | ((data[pos + 1] & 0xF0) >> 4));
-                        dst[2] = 0;
+                        dst[1] = EncLUT[(data[pos + 0] << 4) & 0x3f | (data[pos + 1] >> 4) & 0x0f];
+                        dst[2] = EncLUT[(data[pos + 1] << 2) & 0x3f];
                         break;
                     case 3:
-                        dst[1] = (byte)(((data[pos] & 0x3) << 4) | ((data[pos + 1] & 0xF0) >> 4));
-                        dst[2] = (byte)(((data[pos + 1] & 0xF) << 2) | ((data[pos + 2] & 0xC0) >> 6));
-                        dst[3] = (byte)(data[pos + 2] & 0x3F);
+                        dst[1] = EncLUT[(data[pos + 0] << 4) & 0x3f | (data[pos + 1] >> 4) & 0x0f];
+                        dst[2] = EncLUT[(data[pos + 1] << 2) & 0x3f | (data[pos + 2] >> 6) & 0x03];
+                        dst[3] = EncLUT[(data[pos + 2] << 0) & 0x3f];
                         break;
                 }
                 for (var i = 0; i < numBytesRemain + 1; i++)
                 {
-                    res[curr++] = (char)(dst[i] + 33);
+                    res[curr++] = dst[i];
                     written++;
                     if (insertBr && written == 80 && numBytesRemain == 3)
                     {
@@ -70,11 +84,11 @@ namespace AssParser.Lib
                     }
                 }
                 if (bytes > 1)
-                    res[curr++] = (byte)((src[0] << 2) | (src[1] >> 4));
+                    res[curr++] = (byte)((src[0] << 2) & 0xff | (src[1] >> 4) & 0x03);
                 if (bytes > 2)
-                    res[curr++] = (byte)(((src[1] & 0xF) << 4) | (src[2] >> 2));
+                    res[curr++] = (byte)((src[1] << 4) & 0xff | (src[2] >> 2) & 0x0f);
                 if (bytes > 3)
-                    res[curr++] = (byte)(((src[2] & 0x3) << 6) | (src[3]));
+                    res[curr++] = (byte)((src[2] << 6) & 0xff | (src[3] >> 0) & 0x3f);
             }
             Array.Resize(ref res, curr);
             return res;
