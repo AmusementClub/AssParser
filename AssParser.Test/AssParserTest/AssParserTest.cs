7232f5246b8212f7d9d2bfa38d10f95f919845f0
sun128764 <sun128764@gmail.com>
2023-09-05 15:58:51 +0800

    test: Add AssParserTest

---
+++ AssParser.Test/AssParserTest/AssParserTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssParser.Test.AssParserTest
+{
+    public class AssParserTest
+    {
+        [Theory]
+        [InlineData("1.ass")]
+        [InlineData("2.ass")]
+        public void AssParser_ShouldNot_Throw(string file)
+        {
+            //Arrange
+            var path = Path.Combine("AssParserTest", file);
+
+            //Act
+            var exception = Record.ExceptionAsync(() => Lib.AssParser.ParseAssFile(path));
+
+            //Assert
+            Assert.Null(exception.Result);
+        }
+    }
+}
