54f97775860ca6260492ddd53377ea7c45d9a1fa
sun128764 <sun128764@gmail.com>
2023-09-12 14:47:46 +0800

    chore: remove private path. Auto benchmark.

--- AssParser/Program.cs
+++ AssParser/Program.cs
@@ -8,14 +8,16 @@ namespace AssParser
     {
         static void Main(string[] args)
         {
-            var assfile = Lib.AssParser.ParseAssFile(@"D:\Users\sunjialin\Source\Repos\AssParser\AssParser.Test\AssParserTest\1.ass").Result;
+            var assfile = Lib.AssParser.ParseAssFile(@"[Nekomoe kissaten&VCB-Studio] Cider no You ni Kotoba ga Wakiagaru [Ma10p_1080p][x265_flac].jp&sc.ass").Result;
             var fonts = assfile.UsedFonts();
             foreach (var font in fonts)
             {
                 Console.WriteLine(font.FontName+"\t"+font.UsedChar);
             }
             var txt = assfile.ToString();
+#if !DEBUG
             //var summary = BenchmarkRunner.Run<ParserBenchmark>();
+#endif
             Console.ReadLine();
         }
     }
